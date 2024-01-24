07fc6203faa602815f5930ca8a0b56f008bb452e
Vikas bhatt <96935547+viki8392@users.noreply.github.com>
2024-01-24 12:08:19 +0530

    first commit

---
+++ portifolio/src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/2yxFxMyT/w-app.png',
+    title: "Weather-app",
+    desc: "",
+    link: "",
+    git: "https://github.com/vikibhatt/Weather-app.git",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/76FQRFxk/task-media.png',
+    title: "Task-Media",
+    link: "",
+    git: "https://github.com/vikibhatt/Project-Management-Tool.git",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/02JBp20y/Krypt.png',
+    title: "Krypt",
+    desc: "",
+    link: "",
+    git: "https://github.com/vikibhatt/krypt.git",
+  }
+  
+];
